e2381d704978f8c214fb073f35bff56a3c59a477
Matheus <matheus.souza@indikatore.com>
2024-02-09 15:05:19 -0300

    Criando landing page, instituicao

---
+++ src/components/Team.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export const Team = (props) => {
+  return (
+    <div id="team" className="text-center">
+      <div className="container">
+        <div className="col-md-8 col-md-offset-2 section-title">
+          <h2>MEMBROS</h2>
+          <p>
+          Descubra os rostos e corações por trás da nossa instituição, conheça os membros dedicados que moldam nosso propósito e tornam possíveis nossas missões de amor e serviço à comunidade.
+          </p>
+        </div>
+        <div id="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
+                  <div className="thumbnail">
+                    {" "}
+                    <img src={d.img} alt="..." className="team-img" />
+                    <div className="caption">
+                      <h4 style={{fontWeight: "bold", color:"#ffff"}}>{d.name}</h4>
+                      <p>{d.job}</p>
+                    </div>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/services.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const Services = (props) => {
+  return (
+    <div id="services" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>NOSSOS SERVIÇOS</h2>
+          <p style={{ fontWeight: "bold", color: "#ffff" }}>
+            Explore as realizações que nos definem, descubra as ações que transformam vidas e testemunhe o impacto positivo que deixamos em nossa jornada de amor e serviço.
+          </p>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+              <div key={`${d.name}-${i}`} className="col-md-4" style={{ height: '44%' }}>
+                {" "}
+                <i className={d.icon}></i>
+                <div className="service-desc">
+                  <h3>{d.name}</h3>
+                  <p>{d.text}</p>
+                </div>
+              </div>
+            ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
