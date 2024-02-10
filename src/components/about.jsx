22b5113216003641caf91fb232940ca6d8c7c983
Matheus <matheus.souza@indikatore.com>
2024-02-10 18:56:52 -0300

    Correções de estilo e nomes

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -11,16 +11,16 @@ export const About = (props) => {
           </div>
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
-              <h2>Sobre Nos</h2>
+              <h2>Sobre Nós</h2>
               <p>{props.data ? props.data.paragraph : "loading..."}</p>
-              <h3>Vantagens</h3>
+              <h3>Propósitos</h3>
               <div className="list-style">
                 <div className="col-lg-6 col-sm-6 col-xs-12">
                   <ul>
                     {props.data
                       ? props.data.Why.map((d, i) => (
-                          <li key={`${d}-${i}`}>{d}</li>
-                        ))
+                        <li key={`${d}-${i}`}>{d}</li>
+                      ))
                       : "loading"}
                   </ul>
                 </div>
@@ -28,8 +28,8 @@ export const About = (props) => {
                   <ul>
                     {props.data
                       ? props.data.Why2.map((d, i) => (
-                          <li key={`${d}-${i}`}> {d}</li>
-                        ))
+                        <li key={`${d}-${i}`}> {d}</li>
+                      ))
                       : "loading"}
                   </ul>
                 </div>
